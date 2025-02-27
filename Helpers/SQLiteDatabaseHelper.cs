304dc811008805c60825c42354ef46914585c4cd
Luize Bernardo <luizegomes@live.com>
2025-02-27 18:42:26 -0300

    Inclusão dos métodos Insert, Update, Delete, GetAll e Search.

--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -1,41 +1,40 @@
 ﻿using MauiAppMinhasCompras.Models;
 using SQLite;
+using System.Collections.Generic;
 
-namespace MauiAppMinhasCompras.Helpers
+namespace MauiAppMinhasCompras.Helpers;
+
+public class SQLiteDatabaseHelper
 {
-    public class SQLiteDatabaseHelper
+    readonly SQLiteAsyncConnection _conn;
+    public SQLiteDatabaseHelper(string path)
     {
-        readonly SQLiteAsyncConnection _conn;
-        public SQLiteDatabaseHelper(string path) 
-        { 
-            _conn = new SQLiteAsyncConnection(path);
+        _conn = new SQLiteAsyncConnection(path);
 
-            _conn.CreateTableAsync<Produto>().Wait();
-        }
-        public Task<int> Insert(Produto p)
-        {
-            return _conn.InsertAsync(p);
-        }
-        public Task<List<Produto>> Update(Produto p)
-        {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
-            return _conn.QueryAsync<Produto>(
-            sql, p.Descricao, p.Quantidade, p.Preco, p.Id
-            );
-        }
-        public Task<int> Delete(int id)
-        {
-            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
-        }
-        public Task<List<Produto>> GetAll()
-        {
-            return _conn.Table<Produto>().ToListAsync();
-        }
-        public Task<List<Produto>> Search(string q)
-        {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
-            return _conn.QueryAsync<Produto>(sql);
-        }
+        _conn.CreateTableAsync<Produto>().Wait();
+    }
+    public Task<int> Insert(Produto p)
+    {
+        return _conn.InsertAsync(p);
 
     }
+    public Task<int> Update(Produto p)
+    {
+        string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+        return _conn.ExecuteAsync(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+    }
+    public Task<int> Delete(int id)
+    {
+        return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+    }
+    public Task<List<Produto>> GetAll()
+    {
+        return _conn.Table<Produto>().ToListAsync();
+    }
+    public Task<List<Produto>> Search(string q)
+    {
+        string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + q + "%'";
+        return _conn.QueryAsync<Produto>(sql);
+    }
+
 }
