ae5472aad2a50482a318197c0a4b8181f7815688
Luize Bernardo <luizegomes@live.com>
2025-03-28 19:56:39 -0300

    No arquivo App.xaml.cs foi adicionado a configuração CurrentCulture para a formatação de valores para a moeda local, no nosso caso R$. IsPullToRefreshEnabled="True", utilizado para atualização de página com arrastando para baixo.

--- App.xaml.cs
+++ App.xaml.cs
@@ -24,6 +24,7 @@ namespace MauiAppMinhasCompras
         public App()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
 
             // MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
--- Views/NovoProduto.xaml.cs
+++ Views/NovoProduto.xaml.cs
@@ -22,8 +22,9 @@ public partial class NovoProduto : ContentPage
 
 			await App.Db.Insert(p);
 			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+			await Navigation.PopAsync();
 
-		} catch(Exception ex)
+        } catch(Exception ex)
 		{
 			await DisplayAlert("Ops", ex.Message, "OK");
 		}
